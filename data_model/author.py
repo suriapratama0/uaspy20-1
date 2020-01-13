148aa5caf2a8ab04df7356a3fe39e7c041bfea64
suriapratama0 <56243275+suriapratama0@users.noreply.github.com>
2020-01-13 11:42:25 +0700

    Delete author.py

--- data_model/author.py
+++
@@ -1,9 +0,0 @@
-class Author:
-    def __init__(self, name=""):
-        self.name = name
-        self.gender = ""
-        self.address = ""
-        self.phone = 0
-
-    def toString(self):
-        return "Name: {0}, Address: {1}".format(self.name, self.address)
